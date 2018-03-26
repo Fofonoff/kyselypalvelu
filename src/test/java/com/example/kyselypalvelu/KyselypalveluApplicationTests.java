73d6bb83f74306685c7431a5a76e1a9fe6662403
Fofonoff Joni <a1700398@myy.haaga-helia.fi>
2018-03-26 14:09:41 +0300

    First commit

---
+++ src/test/java/com/example/kyselypalvelu/KyselypalveluApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.kyselypalvelu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KyselypalveluApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
